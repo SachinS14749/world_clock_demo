28b359c403cbca7bae333f01b8d4fc053c8e3346
SachinS14749 <sachin.s14749@gmail.com>
2024-07-25 15:08:40 +0530

    GET call using a world clock open API for demo

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -13,7 +13,8 @@ class _LoadingState extends State<Loading> {
   @override
   void initState() {
     super.initState();
-    getData();
+    // getData();
+    getTestDataTime();
     print('Only code block inside getData() is blocking using await, not code '
         'outside it. That\'s why you are able to see this message first');
   }
@@ -33,6 +34,27 @@ class _LoadingState extends State<Loading> {
     Map map = jsonDecode(response.body);
     print(map);
     print('title key value = ${map['title']}');
+  }
+
+  Future<void> getTestDataTime() async {
+    Uri uri = Uri.parse('http://worldtimeapi.org/api/timezone/Asia/Kolkata');
+    Response response = await get(uri);
+    Map resMap = jsonDecode(response.body);
+    print(resMap);
+    String dateTimeStr = resMap["datetime"];
+    String utcOffsetStr = resMap["utc_offset"];
+    print('$dateTimeStr - $utcOffsetStr');
+    DateTime dateTime = DateTime.parse(dateTimeStr);
+    int hoursDiff = int.parse(utcOffsetStr.substring(1,3));
+    int minDiff = int.parse(utcOffsetStr.substring(4,6));
+    String plusOrMinus = utcOffsetStr.substring(0,1);
+
+    if(plusOrMinus == "+") {
+      dateTime = dateTime.add(Duration(hours: hoursDiff, minutes: minDiff));
+    }else {
+      dateTime = dateTime.subtract(Duration(hours: hoursDiff, minutes: minDiff));
+    }
+    print(dateTime);
   }
 
 }
